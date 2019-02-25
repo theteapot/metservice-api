d56e6cb7f8faccaf53845dfc6493451bd595331b
Taylor Kettle <tin.teapot@gmail.com>
2019-02-25 21:29:02 +1300

    added geojson getting, added metdata tests, server tests

--- src/database/model/metDataModel.js
+++ src/database/model/metDataModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const moment = require('moment')
-const { getPrecipitation } = require('metservice-synoptic-codes')
+const { getPrecipitation, getStation } = require('metservice-synoptic-codes')
 
 const MetDataSchema = require('../schemas/metDataSchema')
 const MetData = mongoose.model('MetData', MetDataSchema)
@@ -22,18 +22,48 @@ async function getCodesPrecipitation ({
 } = {}) {
   let results = await MetData.find({ timestamp: { $gte: start, $lte: stop } })
 
-  console.log(Object.keys(results))
-
   return results.map(({ timestamp, codes }) => {
-    console.log(codes)
-
     codes = codes.map(code => getPrecipitation({ code }))
     return { timestamp, codes }
   })
 }
 
+async function getGeoJSON ({
+  start = new moment().startOf('week').toDate(),
+  stop = new moment().endOf('week').toDate()
+} = {}) {
+  let data = await MetData.find({ timestamp: { $gte: start, $lte: stop } })
+
+  let features = data.reduce((features, { timestamp, codes }) => {
+    for (let code of codes) {
+      let { lat = 0, lng = 0, name = 'No name' } = getStation({ code }) || {}
+
+      let { hoursPrecedingObservation = 0, amountOfPrecipitation = 0 } =
+        getPrecipitation({ code }) || {}
+
+      features.push({
+        type: 'Feature',
+        properties: {
+          timestamp,
+          name,
+          hoursPrecedingObservation,
+          amountOfPrecipitation
+        },
+        geometry: { type: 'Point', coordinates: [lng, lat, 0] }
+      })
+    }
+    return features
+  }, [])
+
+  return {
+    type: 'FeatureCollection',
+    features
+  }
+}
+
 module.exports = {
   saveCodes,
   getCodes,
+  getGeoJSON,
   getCodesPrecipitation
 }
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const routes = require('./routes')
+const { connect, disconnect } = require('../database/index')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,14 +10,16 @@ routes(app)
 
 let server
 
-function start () {
+async function start () {
+  await connect()
   server = app.listen(port, () =>
     console.log(`MetService listening on port ${port}!`)
   )
   return server
 }
 
-function stop () {
+async function stop () {
+  await disconnect()
   return server.close()
 }
 
